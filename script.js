9e4120baff0334176cb8a10eb9f9731bd2a3cfbd
AEmin321 <ahmademin321@gmail.com>
2023-03-08 18:59:32 +0300

    Added the gamemanager function to play a round and switch players

--- script.js
+++ script.js
@@ -6,14 +6,25 @@ function Gameboard () {
   for (let i=0;i<rows;i++) {
     gameboard[i]=[];
     for (let j=0;j<column;j++) {
-      gameboard[j].push(cell());
+      gameboard[i].push(cell());
     }
+  }
+
+  const getBoard=()=>gameboard;
+
+  const dropToken= (row,column,player)=> {
+    if (gameboard[row][column].getValue()!==0) {
+      return ;
+    }
+    gameboard[row][column].setValue(player);
   }
 
   const printBoard =()=> {
     const printWithValues=gameboard.map((row)=>row.map((cell)=>cell.getValue()));
-    console.log (printWithValues);
+    console.log(printWithValues);
   }
+
+  return {dropToken,printBoard,getBoard};
 }
 
 function cell () {
@@ -25,3 +36,42 @@ function cell () {
   const getValue=()=>value;
   return {setValue,getValue};
 }
+
+function gameManager () {
+  const players=[
+    {
+      name:'Player1',
+      token:'X'
+    },
+    {
+      name:'Player2',
+      token:'O'
+    }
+  ];
+
+  const board=Gameboard();
+
+  let activePlayer=players[0];
+  const getActivePlayer=()=>activePlayer;
+
+  const switchActivePlayer=()=>{
+    activePlayer=activePlayer===players[0]?players[1]:players[0];
+  }
+
+  const playRound=(row,column)=>{
+    board.dropToken(row,column,activePlayer.token);
+    printNewRound();
+  }
+
+  const printNewRound=()=>{
+    board.printBoard();
+    console.log (`${getActivePlayer().name}`);
+  };
+
+  playRound(0,0);
+  switchActivePlayer();
+  printNewRound();
+  playRound(0,0);
+}
+
+const game=gameManager();
