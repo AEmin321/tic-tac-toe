119730926183c613936e3558497337dd00af4856
AEmin321 <ahmademin321@gmail.com>
2023-03-09 23:13:30 +0300

    Deleted the cell function and fixed board array to use combination array for checking

--- script.js
+++ script.js
@@ -3,45 +3,35 @@ function Gameboard () {
   const gameboard=[];
 
   for (let i=0;i<cells;i++) {
-    gameboard.push(cell());
+    gameboard.push(0);
     
   }
 
   const getBoard=()=>gameboard;
 
   const dropToken= (cell,player)=> {
-    if (gameboard[cell].getValue()!==0) {
+    if (gameboard[cell]!==0) {
       return ;
     }
-    gameboard[cell].setValue(player);
+    gameboard[cell]=player;
   }
 
   const printBoard =()=> {
-    const printWithValues=gameboard.map((cell)=>cell.getValue());
+    const printWithValues=gameboard.map((cell)=>cell);
     console.log(printWithValues);
   }
 
   return {dropToken,printBoard,getBoard};
 }
 
-function cell () {
-  let value=0;
-  const setValue=(player)=> {
-    value=player;
-  }
-
-  const getValue=()=>value;
-  return {setValue,getValue};
-}
-
 function gameManager () {
   const players=[
     {
-      name:'Player1',
+      name:'You ara X',
       token:'X'
     },
     {
-      name:'Player2',
+      name:'You are O',
       token:'O'
     }
   ];
@@ -49,18 +39,36 @@ function gameManager () {
   const board=Gameboard();
 
   let activePlayer=players[0];
-  const getActivePlayer=()=>activePlayer;
 
   const switchActivePlayer=()=>{
-    activePlayer=activePlayer===players[0]?players[1]:players[0];
+    activePlayer=activePlayer===players[0] ? players[1] : players[0];
   }
 
+  const getActivePlayer=()=>activePlayer;
+
+
+  const checkWin=(board,player)=>{
+    const winningCombos = [
+      [0, 1, 2], [3, 4, 5], [6, 7, 8], // rows
+      [0, 3, 6], [1, 4, 7], [2, 5, 8], // columns
+      [0, 4, 8], [2, 4, 6] // diagonals
+     ];
   
+     for (let combo of winningCombos) {
+      const [a,b,c] = combo;
+      console.log (board[a]);
+       if (board[a] && board[a] === board[b] && board[a] === board[c]) {
+         return true; // combination found
+       }
+     }
+  
+     return false; // no combination found 
+  }
 
   const playRound=(cell)=>{
     board.dropToken(cell,activePlayer.token);
     switchActivePlayer();
-    console.log(checkWin(board.getBoard(),'X'));
+    console.log(checkWin(board.getBoard(),activePlayer.token));
   }
 
   const printNewRound=()=>{
@@ -77,18 +85,18 @@ function screenManager () {
 
   const switchPlayerDiv=document.querySelector('.active-player');
   const boardDiv=document.querySelector('.board');
-  const activePlayer=game.getActivePlayer();
+  let activePlayer=game.getActivePlayer().name;
   
   const updateGame=()=>{
     boardDiv.textContent='';
-    switchPlayerDiv.textContent=`${activePlayer.name}'s turn.`;
+    switchPlayerDiv.textContent=`${activePlayer}'s turn.`;
     const board=game.getBoard();
     
     board.forEach((cell,indexc) => {
       const cellDiv=document.createElement('div');
       cellDiv.dataset.cell=indexc;
       cellDiv.classList.add('cell');
-      cellDiv.textContent=cell.getValue();
+      cellDiv.textContent=cell;
       boardDiv.appendChild(cellDiv);
     });
   }
